098be1dbaa1f12af751e15bbb8656e7bd64b74ac
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-04-29 18:19:28 +0100

    do not encode value if respond throws

--- index.js
+++ index.js
@@ -94,7 +94,9 @@ module.exports = class ProtomuxRPC extends EventEmitter {
 
       this._responding--
 
-      if (responseEncoding && id) value = c.encode(responseEncoding, value)
+      if (!error && responseEncoding && id) {
+        value = c.encode(responseEncoding, value)
+      }
     }
 
     if (id) this._response.send({ id, error, value })
