3e8ca2f3c39c4d917645d51473d9a3a8a90f6758
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-06-02 14:17:46 +0200

    Use `buffer` as default value encoding

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = class ProtomuxRPC extends EventEmitter {
 
     const {
       id,
-      valueEncoding,
+      valueEncoding = c.buffer,
       handshake,
       handshakeEncoding
     } = options
